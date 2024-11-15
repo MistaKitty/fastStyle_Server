c952f59ab6d3cfc58e545309caecb70c1393dfac
Tiago <tiago.abreu.laibacas@gmail.com>
2024-11-15 12:22:27 +0000

    📦 Added cors and email-validator dependencies

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 
@@ -8,6 +9,22 @@ dotenv.config();
 const app = express();
 
 connectDB();
+
+const allowedOrigins = [process.env.API_LOCAL, process.env.API_REMOTE];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
